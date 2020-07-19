407d01c969012f5aba39fbb1c8a64ae8ac11f204
antony-guthiga <toniguthiga93@gmail.com>
2020-07-19 11:19:14 +0300

    add error handling view function

--- app/views.py
+++ app/views.py
@@ -8,3 +8,7 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
