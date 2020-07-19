4ef83e314860babfb5df9f05f568b604c03f0670
antony-guthiga <toniguthiga93@gmail.com>
2020-07-19 15:49:52 +0300

    update virtual library

---
+++ app/main/views.py
@@ -0,0 +1,11 @@
+from flask import render_template
+from . import main
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
