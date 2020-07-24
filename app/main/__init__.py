3d06134a5883e6ad547a7a689898bb6d0ce03e2b
antony-guthiga <toniguthiga93@gmail.com>
2020-07-24 18:22:20 +0300

    add photo instance

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,8 @@
 from flask import Blueprint
 main = Blueprint('main',__name__)
 from . import views,errors
+
+# Adding the Permission class to the template context
+# @main.app_context_processor
+# def inject_permissions():
+#     return dict(Permission=Permission)
--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moring:Access@localhost/blogs'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SECRET_KEY = '~\xb4\xc2\xff\x9fuZ\xa4D\xbe\x7f\xca'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     pass
